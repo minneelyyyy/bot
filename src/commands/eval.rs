4de3d738a2f92061d01158ec6de24bf0617b4361
minneelyyyy <minneelyyyy@proton.me>
2024-10-19 23:13:05 -0400

    no stdout hooking

--- src/commands/eval.rs
+++ src/commands/eval.rs
@@ -10,10 +10,7 @@ pub async fn eval(ctx: Context<'_>,
 	let expr = expr.strip_prefix("```")
 		.and_then(|s| s.strip_suffix("```")).unwrap_or(&expr);
 
-	let mut output = Vec::new();
-	let writer = Cursor::new(&mut output);
-
-	let runtime = lamm::Runtime::new(Cursor::new(expr)).stdout(writer);
+	let mut runtime = lamm::Runtime::new(Cursor::new(expr));
 
 	let values = runtime.values().fold(Ok(String::new()), |acc, v| {
 		if acc.is_err() {
